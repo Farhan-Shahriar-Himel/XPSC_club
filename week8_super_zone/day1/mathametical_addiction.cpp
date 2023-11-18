ae2ccd3a060a16fc5296c9e9a1cbe93ed1d1d611
Farhan-Shahriar-Himel <129546333+Farhan-Shahriar-Himel@users.noreply.github.com>
2023-11-18 23:21:55 +0600

    week 8 uploaded

---
+++ week8_super_zone/day1/mathametical_addiction.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(NULL);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int _; cin >> _;
+
+    while (_--) {
+
+        int u, v; cin >> u >> v;
+
+        cout << (-(u * u)) << " " << (v * v) << "\n";
+
+    }
+
+    return 0;
+
+}
